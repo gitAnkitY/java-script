d3a7c67dca050d73ad575997be5936059bc5bddd
gitAnkitY <ankityadav06092006@gmail.com>
2025-08-07 02:28:28 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all code as newer version of JS
+
+// alert( 3 + 3 ) // we are using node js not browser
+
+
+let name ="ankit"
+let age= 18
+let isLoggedIn= false
+let state;
+
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined =>
+// symbol=> unique
+
+
+// object
+
+
+console.log(typeof "ankit");
+
+console.log(typeof null); // object
