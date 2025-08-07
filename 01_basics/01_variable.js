6293d1fb01879ba4543db0ad57265917962c4ac8
gitAnkitY <ankityadav06092006@gmail.com>
2025-08-07 01:52:51 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId =12345
+let accountEmail = "Ankit@12"
+var accountPass ="0987"
+accountCity = "noida"
+
+// accountId= 2342 // not Allowed
+
+accountEmail = "Ankit12@"
+accountPass ="12123"
+accountCity= "mumbai"
+
+// using var is not good in javaScript//
+
+console.table([accountId, accountEmail, accountPass, accountCity])
