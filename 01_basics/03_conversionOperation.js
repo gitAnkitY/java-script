92897d2432f3b2e834b474e800a33cc2b0dc267f
gitAnkitY <ankityadav06092006@gmail.com>
2025-08-08 01:08:21 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let number= "33"
+let num="33abc"
+let valueInNumber=Number(num)
+console.log(typeof number)
+console.log(typeof num )
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+
+// 33 => 33
+// "33" => string
+// "33abc" => NaN (not a number)
+// true => 1; false=> 0;
+// null => 0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1=> true; 0=>fakse
+// ""=> false
+// "ankit"=> true
