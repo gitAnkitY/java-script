3bae1ab737ed13ee3fd13d367c7f24ce749ccd62
gitAnkitY <ankityadav06092006@gmail.com>
2025-08-21 10:31:08 +0000

    Array Basics

---
+++ Array/Array_01.js
@@ -0,0 +1,16 @@
+// Array //
+// Array is a linear collection of things 
+
+// syntax=>>   let name =["Ankit", "Anand", "preeti"]
+
+let name =["Ankit", "Anand", "preeti"]
+
+console.log(name.length) // to know the length of the array
+
+console.log(name[0][0]) // method chaining also work here 
+
+// Array is Mutable in JavaScript
+
+let arr = [1, 2, 3];
+arr[0] = 100;   // change first element
+console.log(arr); // [100, 2, 3]
