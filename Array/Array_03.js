4087c700e04d34656d807f727498eabb33ce0037
gitAnkitY <ankityadav06092006@gmail.com>
2025-08-22 16:14:01 +0000

    Index Of & Includes

---
+++ Array/Array_03.js
@@ -0,0 +1,19 @@
+// ( index of )
+// return index of something 
+
+let primary=["red","yellow","blue"];
+primary.indexOf("yellow")
+//1
+primary.indexOf("green")
+//-1
+primary.indexOf("Yellow")
+// -1
+
+
+// includes : search for a value 
+
+primary.includes("red");
+// true 
+
+primary.includes("green");
+// false 
