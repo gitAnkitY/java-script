121b9388b9165ba2c24d8a3ef7fc494273080db9
gitAnkitY <ankityadav06092006@gmail.com>
2025-08-22 16:55:44 +0000

    concat & reverse

---
+++ Array/Array_04.js
@@ -0,0 +1,13 @@
+// (CONCAT) : merge two array
+let primary=["red", "yellow", "blue"]
+let secondary=["orange","green","violet"]
+
+ let newArray=primary.concat(secondary);
+
+ console.log(newArray)
+
+ // ***(reverse)***
+ // reverse an array
+
+ let Arrayone=primary.reverse();
+ console.log(Arrayone);
