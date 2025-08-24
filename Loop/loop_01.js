e33be50d77108fa815c8b4d554c009ec67e61d5e
gitAnkitY <ankityadav06092006@gmail.com>
2025-08-24 10:11:28 +0000

    Loop basic

---
+++ Loop/loop_01.js
@@ -0,0 +1,41 @@
+// (LOOP)
+
+//for Loop
+
+// for (initialization; condition; afterthought)
+//  statement
+
+for(let i=0;i<=5;i++){
+    console.log(i);
+}
+
+//  do while loop
+// do
+//  statement
+//while (condition);
+
+let i = 0;
+do {
+  i += 1;
+  console.log(i);
+} while (i < 5);
+
+// while loop 
+// while (condition)
+//  statement
+
+let n = 0;
+let x = 0;
+while (n < 3) {
+  n++;
+  x += n;
+  console.log(n)
+}
+
+
+//INFINITE LOOP
+// Infinite loops are bad!
+
+//while (true) {
+//  console.log("Hello, world!");
+//  }
