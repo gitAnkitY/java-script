f576116df89d7f44b1b2792e4389aa80d5394ae2
gitAnkitY <ankityadav06092006@gmail.com>
2025-08-28 10:30:48 +0000

    Array of object

---
+++ ObjectL/part_04.js
@@ -0,0 +1,23 @@
+// ***********(ARRAY OF OBJECTS)*********
+
+// syntax=> just use square bracket instead of curly ones 
+
+const students=[
+    {
+        name: "ankit",
+        grade:"A+",
+        city:"delhi"
+    },
+    {
+        name:"anand",
+        grade:"A",
+        city:"mumbai"
+    },
+    {
+        name:"preeti",
+        grade:"A",
+        city:"kolkata"
+    }
+];
+
+console.log(students);
