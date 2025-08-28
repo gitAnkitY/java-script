7ec5bced5f38bb25b74083643c35b35d7312c7a3
gitAnkitY <ankityadav06092006@gmail.com>
2025-08-28 11:20:56 +0000

    math methods

---
+++ ObjectL/part_05.js
@@ -0,0 +1,29 @@
+// Math 
+//properties
+console.log(Math.PI)
+console.log(Math.E)
+
+// methods
+
+console.log(Math.abs(-5));
+
+console.log(Math.floor(5.5));
+
+console.log(Math.ceil(5.5));
+
+console.log(Math.random());
+
+//Random integer
+
+// step1 
+
+let num=Math.random();
+
+//step2
+num=num*10;
+
+//step3
+num=Math.floor(num);
+
+//step4
+num=num+1;
