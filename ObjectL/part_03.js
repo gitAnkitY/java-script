01b1e814da8c5c93300790deaa313b67dacbeb45
gitAnkitY <ankityadav06092006@gmail.com>
2025-08-28 10:22:40 +0000

    Object of Objects

---
+++ ObjectL/part_03.js
@@ -0,0 +1,22 @@
+// OBJECTS OF OBJECTS **********(nesting of objects)********
+
+// syntax 
+
+// storing multiple info in one 
+
+const students={
+    ankit:{
+        grade :"A",
+        City:"delhi"
+    },
+    Anand:{
+        grade:"b",
+        city: "mumbai",
+    },
+    preeti:{
+        grade:"C",
+        city:"kolkata"
+    }
+};
+
+console.log(students);
