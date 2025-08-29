227fca1b296d6ac3b33ebd30b57980fe7cb210d2
gitAnkitY <ankityadav06092006@gmail.com>
2025-08-29 10:28:39 +0000

    Questions

---
+++ ObjectL/Question.js
@@ -0,0 +1,15 @@
+// Question 
+// generate a random number between 1 and 100
+
+console.log(Math.floor(Math.random()*100+1));
+
+// we can get the range here by multiplying the random number by 100 
+
+// Question 2
+// generate a random number between 1 to 5 
+
+console.log(Math.floor(Math.random()*5+1));
+
+// lets take an example if we need a random number between 21 to 25 so we can do like 
+
+console.log(Math.floor(Math.random()*5+20));
