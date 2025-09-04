93a195ef5b5af4b39d053aefd6c8c24aca76c3c8
gitAnkitY <ankityadav06092006@gmail.com>
2025-09-04 09:51:13 +0000

    setInterval

---
+++ js_07/setInterval.js
@@ -0,0 +1,11 @@
+// ***********(SET INERVAL)*****
+//setInterval() is a built-in function that repeatedly calls another
+//  function after a fixed time delay (in milliseconds).
+
+// syntax : setInterval(function , timeout);
+
+let id =setInterval(()=>{
+    console.log("ankit");
+},2000)
+
+console.log(id);
