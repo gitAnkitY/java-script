697bc5a638c46a0eea774fce5007a60222aa6275
gitAnkitY <ankityadav06092006@gmail.com>
2025-09-04 09:39:32 +0000

    setTimeout

---
+++ js_07/setTimeout.js
@@ -0,0 +1,13 @@
+//*******(SET TIMEOUT)*******//
+// The setTimeout() method of the Window interface sets a timer which
+//  executes a function or specified piece of code once the timer expires.
+
+// syntax : setTimeout(function, timeout);
+
+console.log("hi there");
+
+setTimeout(()=>{
+    console.log("home!");
+},4000);
+
+console.log("welcome");
