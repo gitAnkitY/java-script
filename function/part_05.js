2aaa15c2227b1c517ba68ef474ba8978e6a1ce76
gitAnkitY <ankityadav06092006@gmail.com>
2025-09-04 07:55:28 +0000

    higher order function

--- function/part_05.js
+++ function/part_05.js
@@ -25,3 +25,30 @@ multipleGreet(greet,10);
 
 // Example: that returns a function 
 
+function OddEvenTest(request){
+    if (request=="odd"){
+        let odd= function(n){
+            console.log(!(n%2 == 0));
+        }
+        return odd;
+    }
+    
+    else if(request=="even"){
+        let even=function(n){
+            console.log(n%2==0);
+        }
+        return even;
+    }
+    else {
+        console.log("wrong request");
+    }
+}
+
+let request = "even";
+
+// Get the function based on request
+let check = OddEvenTest(request);
+
+// Now call the returned function with a number
+check(5);   // 👉 false (since 5 is not even)
+
